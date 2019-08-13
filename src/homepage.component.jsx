0d98cf899a27cb00c54a1fc333154f4069e2ff55
Levi Strange <levi.strange@clickon.co>
2019-08-13 20:42:10 +0100

    intial commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './homepage.styles.scss'
+// Fcntional component because we dont need
+// any lifecycle methods, nor do we need to 
+// store state.
+const Homepage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">HATS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">TOPS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">SHOES</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">JEANS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">SOCKS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Homepage;

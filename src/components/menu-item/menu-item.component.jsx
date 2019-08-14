107c37ca2df86b98a38c0c5a0f3fb2f1504417ea
Levi Strange <levi.strange@clickon.co>
2019-08-14 10:54:05 +0100

    added hover, components and styles

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 
 import './menu-item.styles.scss'
 
-const MenuItem = () => (
+const MenuItem = ({title, imageUrl, size}) => (
 
-    <div className="menu-item">
-    <div className="content">
-        <h1 className="title">HATS</h1>
-        <span className="subtitle">SHOP NOW</span>
+    <div className={`${size} menu-item`}>
+        <div style={{
+            backgroundImage: `url(${imageUrl})`
+            }} 
+            className="background-image">
+
+        </div>
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
     </div>
-</div>
 )
 
    
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,42 +1,13 @@
 import React from 'react'
 import './homepage.styles.scss'
+
+import Directory from '../../components/directory/directory.component'
 // I used a Functional component because
 // we dont need any lifecycle methods
 // nor do we need to store state.
 const Homepage = () => (
     <div className="homepage">
-        <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">HATS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">TOPS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">SHOES</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">JEANS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">SOCKS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-        </div>
+       <Directory/>
     </div>
 );
 

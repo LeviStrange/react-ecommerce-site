9db584d82da7ed3ff9f2128533d96a4be207aa83
Levi Strange <levi.strange@clickon.co>
2019-08-15 11:11:47 +0100

    added in react-router

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './homepage.styles.scss'
-
 import Directory from '../../components/directory/directory.component'
+
 // I used a Functional component because
 // we dont need any lifecycle methods
 // nor do we need to store state.

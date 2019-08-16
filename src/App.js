0d2624b9e59ae2ab228c95a67021336b74b62a34
Levi Strange <levi.strange@clickon.co>
2019-08-16 13:08:54 +0100

    signin comps and page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 //Components
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
+
 import Header from './components/header/header.component'
 
 //Styles
@@ -15,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInSignUp}/>
       </Switch>
     </div>
   );
